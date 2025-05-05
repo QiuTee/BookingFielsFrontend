2cc1ba78f618901e8f721368a3b04af104c081b4
QiuTee <nqt8977@gmail.com>
2025-05-05 22:57:13 +0700

    add

--- Frontend/field-booking-app/src/pages/Bookingpage.jsx
+++ Frontend/field-booking-app/src/pages/Bookingpage.jsx
@@ -20,7 +20,6 @@ export default function BookingPage() {
     
     return (
         <div className="min-h-screen bg-gray-100 p-6">
-            <StepProgress currentStep={step} />
             {step === 1 && <TimeSelection fieldId={fieldId} nextStep={() => setStep(2)} />}
             {step === 2 && <BookingForm prevStep={() => setStep(1)} nextStep={() => setStep(3) } />}
             {step === 3 && <BookingSummary prevStep={() => setStep(2)} />}
